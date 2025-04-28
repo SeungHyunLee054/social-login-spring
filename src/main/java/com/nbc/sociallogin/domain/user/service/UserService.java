30f5b318ecd79adf42d9f6656e55aa02b27341c2
SeungHyunLee054 <yellow13lsh@naver.com>
2025-04-28 21:25:55 +0900

    Refactor : 불필요한 transaction 제거 및 import 구조 수정
    불필요하게 남용되던 transaction 어노테이션 제거, 패키지 위치에 따른 import를 할때 계층이 맞지 않았던 문제를 개선

--- src/main/java/com/nbc/sociallogin/domain/user/service/UserService.java
+++ src/main/java/com/nbc/sociallogin/domain/user/service/UserService.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nbc.sociallogin.domain.auth.vo.AuthUser;
+import com.nbc.sociallogin.domain.user.dto.UserInfo;
 import com.nbc.sociallogin.domain.user.entity.User;
 import com.nbc.sociallogin.domain.user.enums.RegistrationType;
 import com.nbc.sociallogin.domain.user.enums.UserRole;
 import com.nbc.sociallogin.domain.user.repository.UserRepository;
-import com.nbc.sociallogin.infrastructure.oauth.dto.response.OAuthUserInfoResponse;
-import com.nbc.sociallogin.presentation.user.dto.UserResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,26 +18,23 @@ public class UserService {
 	private final UserRepository userRepository;
 
 	@Transactional
-	public User findOrCreate(OAuthUserInfoResponse userInfoResponse) {
-		return userRepository.findByEmail(userInfoResponse.getEmail())
-			.orElseGet(() -> createNewOAuthUser(userInfoResponse));
+	public User findOrCreate(UserInfo userInfo) {
+		return userRepository.findByEmail(userInfo.getEmail())
+			.orElseGet(() -> createNewOAuthUser(userInfo));
 	}
 
-	public UserResponse getUser(AuthUser authUser) {
-		User user = userRepository.findById(authUser.getId())
+	public User getUser(AuthUser authUser) {
+		return userRepository.findById(authUser.getId())
 			.orElseThrow(() -> new RuntimeException("User not found"));
-
-		return UserResponse.from(user);
 	}
 
-	@Transactional
-	protected User createNewOAuthUser(OAuthUserInfoResponse userInfoResponse) {
+	protected User createNewOAuthUser(UserInfo userInfo) {
 		User user = User.builder()
-			.email(userInfoResponse.getEmail())
-			.name(userInfoResponse.getName())
-			.birth(userInfoResponse.getBirth())
-			.phone(userInfoResponse.getPhone())
-			.registrationType(RegistrationType.from(userInfoResponse.getOAuthProvider()))
+			.email(userInfo.getEmail())
+			.name(userInfo.getName())
+			.birth(userInfo.getBirth())
+			.phone(userInfo.getPhone())
+			.registrationType(RegistrationType.from(userInfo.getOAuthProvider()))
 			.userRole(UserRole.ROLE_USER)
 			.build();
 
