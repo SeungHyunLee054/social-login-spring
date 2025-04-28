f0fafe3004fc9b17c319962b0c851944c36acd3e
SeungHyunLee054 <yellow13lsh@naver.com>
2025-04-28 15:30:21 +0900

    Refactor : enum 값 찾는 부분 로직 변경
    for-if를 통해 찾지 않고 stream을 사용해 코드 가독성을 높임

--- src/main/java/com/nbc/sociallogin/domain/user/enums/RegistrationType.java
+++ src/main/java/com/nbc/sociallogin/domain/user/enums/RegistrationType.java
@@ -1,17 +1,16 @@
 package com.nbc.sociallogin.domain.user.enums;
 
+import java.util.Arrays;
+
 import com.nbc.sociallogin.infrastructure.oauth.type.OAuthProvider;
 
 public enum RegistrationType {
-	DOMAIN,KAKAO;
+	DOMAIN, KAKAO;
 
 	public static RegistrationType from(OAuthProvider oAuthProvider) {
-		for (RegistrationType registrationType : RegistrationType.values()) {
-			if (registrationType.name().equalsIgnoreCase(oAuthProvider.name())) {
-				return registrationType;
-			}
-		}
-
-		return DOMAIN;
+		return Arrays.stream(RegistrationType.values())
+			.filter(r -> r.name().equalsIgnoreCase(oAuthProvider.name()))
+			.findFirst()
+			.orElse(DOMAIN);
 	}
 }
